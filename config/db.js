4377ef14d9ec920833d9b8fd079904c882c4b689
syedrafayjavaid <syedrafayjavaid16@gmail.com>
2023-01-08 05:57:11 +0500

    uu

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
 
     console.log("The url is", process.env.LOCAL_MONGODB_URI);
     try {
-        const connect = await mongoose.connect(process.env.LOCAL_MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        const connect = await mongoose.connect('mongodb://127.0.0.1:27017/SMS', { useNewUrlParser: true, useUnifiedTopology: true });
         console.log(`MongoDB  successfully connected to ${connect.connection.host}`)
     } catch (error) {
         console.log('Connection could not be created, possible cause: ' + error.message);
